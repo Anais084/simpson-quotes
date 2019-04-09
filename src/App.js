7d0b50dfb9b5d765970f5511e57da3182813488e
Anais <anais.chevallier@gmail.com>
2019-04-09 23:05:37 +0200

    updating files for quest n°3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true
+    };
+  }
+
+    handleClick = () => {
+      this.setState({working: !this.state.working});
+    };
+  
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={this.state.working ? "App-logo":"App-logo-spin"} alt="logo" />
+          <button onClick={this.handleClick}>Working?</button>
+          <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+        <Lamp on />
+        <Lamp />
+        <Quotes />
       </div>
     );
   }
